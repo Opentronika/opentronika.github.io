18e00c64cbcd6a020738460782ad50713a5e49f7
BuildTools <unconfigured@null.spigotmc.org>
2023-08-08 22:43:45 -0700

    add multilanguage support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(),
     sitemap({
         customPages: ['https://opentronika.org/lcd-editor-pro/'],
-      })],
+      }),
+      astroI18next()],
   site:'https://opentronika.org',
 });
