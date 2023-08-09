651cd03e32324beae2dca647a5d4111d12a01f02
BuildTools <unconfigured@null.spigotmc.org>
2023-08-08 22:43:15 -0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),
+    sitemap({
+        customPages: ['https://opentronika.org/lcd-editor-pro/'],
+      })],
   site:'https://opentronika.org',
 });
