fbd254353f8f0545b0d55d0b9ee8dbca9a50afbd
lutgaru <lutgaru@gmail.com>
2025-04-22 22:59:52 -0700

    update packets and refactor i18n to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
-import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(),
-    sitemap({
-        customPages: ['https://opentronika.org/lcd-editor-pro/'],
-      }),
-      astroI18next()],
-  site:'https://opentronika.org',
+  sitemap({
+    customPages: ['https://opentronika.org/lcd-editor-pro/'],
+  })],
+  i18n: {
+    locales: ["en", "es"], // Locales you want to support
+    defaultLocale: "en", // Default locale (fallback)
+    routing: {
+      prefixDefaultLocale: true, // Ensures that your default locale is prefixed aswell
+    },
+  },
+  site: 'https://opentronika.org',
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+    en: 'English',
+    es: 'Spanish',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.Tools': 'Tools',
+        "description": "A space to learn electronics",
+        "lcd-editor-pro": "Advanced and powerful LCD characters editor online. Generally, LCD character editors only support one character, this tool supports multiple characters so you can make complex designs directly as a real LCD."
+    },
+    es: {
+        'nav.home': 'Inicio',
+        'nav.Tools': 'Herramientas',
+        "description": "Un sitio para aprender electronica",
+        "lcd-editor-pro": "Poderoso y avanzado editor de caracteres LCD online. Los editores para caracteres LCD solo soportan un carácter, esta herramienta soporta múltiples caracteres con lo que podrás hacer diseños complejos directamente como si fuera una lcd real. "
+    },
+} as const;
