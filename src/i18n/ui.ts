b23b864716b113089db0d2564435a560f36e697b
lutgaru <lutgaru@gmail.com>
2025-04-28 22:19:12 -0700

    update font of name and slogan

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,13 +9,13 @@ export const ui = {
     en: {
         'nav.home': 'Home',
         'nav.Tools': 'Tools',
-        "description": "A space to learn electronics",
+        "description": "A space to learn electronics.",
         "lcd-editor-pro": "Advanced and powerful LCD characters editor online. Generally, LCD character editors only support one character, this tool supports multiple characters so you can make complex designs directly as a real LCD."
     },
     es: {
         'nav.home': 'Inicio',
         'nav.Tools': 'Herramientas',
-        "description": "Un sitio para aprender electronica",
+        "description": "Un sitio para aprender electronica.",
         "lcd-editor-pro": "Poderoso y avanzado editor de caracteres LCD online. Los editores para caracteres LCD solo soportan un carácter, esta herramienta soporta múltiples caracteres con lo que podrás hacer diseños complejos directamente como si fuera una lcd real. "
     },
 } as const;
