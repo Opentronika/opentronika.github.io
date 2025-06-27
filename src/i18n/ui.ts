4a27f18f92fe0f169983590ba2d4d992561d34b4
lutgaru <lutgaru@gmail.com>
2025-06-26 19:14:26 -0700

    Add SerialGUI-rs tool and separate tools from main page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,12 +10,14 @@ export const ui = {
         'nav.home': 'Home',
         'nav.Tools': 'Tools',
         "description": "A space to learn electronics.",
-        "lcd-editor-pro": "Advanced and powerful LCD characters editor online. Generally, LCD character editors only support one character, this tool supports multiple characters so you can make complex designs directly as a real LCD."
+        "lcd-editor-pro": "Advanced and powerful LCD characters editor online. Generally, LCD character editors only support one character, this tool supports multiple characters so you can make complex designs directly as a real LCD.",
+        "serialgui-rs": "SerialGUI-Rs is a fast and efficient cross-platform serial terminal monitor written in Rust. It features a lightweight graphical interface for real-time monitoring of serial ports. Users can configure parameters like baud rate and parity easily, making it ideal for developers working with embedded systems and serial communication across multiple platforms.",
     },
     es: {
         'nav.home': 'Inicio',
         'nav.Tools': 'Herramientas',
         "description": "Un sitio para aprender electronica.",
-        "lcd-editor-pro": "Poderoso y avanzado editor de caracteres LCD online. Los editores para caracteres LCD solo soportan un carácter, esta herramienta soporta múltiples caracteres con lo que podrás hacer diseños complejos directamente como si fuera una lcd real. "
+        "lcd-editor-pro": "Poderoso y avanzado editor de caracteres LCD online. Los editores para caracteres LCD solo soportan un carácter, esta herramienta soporta múltiples caracteres con lo que podrás hacer diseños complejos directamente como si fuera una lcd real. ",
+        "serialgui-rs": "SerialGUI-Rs es un monitor de terminal serial multiplataforma, rápido y eficiente, escrito en Rust. Ofrece una interfaz gráfica liviana para la monitorización en tiempo real de puertos seriales. Permite configurar parámetros como la velocidad en baudios y la paridad, siendo ideal para desarrolladores que trabajan con sistemas embebidos y comunicación serial.",
     },
 } as const;
